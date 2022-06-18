33c8759c4bc92fad25b8461401db265b12c6bed2
Mridul <gmridul556@gmail.com>
2022-06-18 12:11:04 +0530

    Backend Created

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const app = express();
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
 
 dotenv.config();
+
+app.use(express.json());
+
 mongoose 
  .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
@@ -11,6 +16,9 @@ mongoose
  .then(() => console.log("MongoDB connected!"))
  .catch(err => console.log(err));
 
-app.listen(8800, ()=>{
-    console.log("Server is running ")
-})
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
